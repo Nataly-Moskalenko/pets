139f2d05edbb125229bd24064003d5fee352469b
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-08-27 21:40:40 +0300

    fixes deploy

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={jost.className} style={{ display: 'flex' }}>
         <Providers>
           <Sidebar />
